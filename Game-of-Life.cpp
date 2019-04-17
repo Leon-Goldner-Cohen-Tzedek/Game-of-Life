7dddaac6945d1113245d514a0a0c6d542d5abeb3
Leon Katz <leonkatz21@gmail.com>
2019-04-16 18:36:21 -0700

    Basic functionality is done and tested I think, next step is to have a file fed setup

--- Game-of-Life.cpp
+++ Game-of-Life.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 using namespace std;
 #define GAME_BOARD_SIZE 12
+#define GEN_NUMBER 3
 
-void one_gen(int game_board[][GAME_BOARD_SIZE], int newGB[][GAME_BOARD_SIZE ]);
-void record(int newGB[][GAME_BOARD_SIZE], int x, int y, int cells_number);//records the dead or alive cells
-void transfer(int game_board[][GAME_BOARD_SIZE], int newGB[][GAME_BOARD_SIZE]);
 // one_gens every single cell's neighbours, returns the "cell's number"
 //       Cell's number is the sum of all the cells around it. +1 for all the alive cells, +0 for all the dead cells
 
@@ -18,67 +16,78 @@ void transfer(int game_board[][GAME_BOARD_SIZE], int newGB[][GAME_BOARD_SIZE]);
 
 int main ()
 {
-  int game_board[GAME_BOARD_SIZE][GAME_BOARD_SIZE];
-  int newGB[GAME_BOARD_SIZE][GAME_BOARD_SIZE];
+  bool game_board[GAME_BOARD_SIZE][GAME_BOARD_SIZE];
+  bool newGB[GAME_BOARD_SIZE][GAME_BOARD_SIZE];
+
   bool game_over = 0;
 
-  while (game_over != 1)
+  // for (int x = 0; x < GAME_BOARD_SIZE; x++)
+  // {
+  //   for (int y = 0; y < GAME_BOARD_SIZE; y++)
+  //   {
+  //     game_board[x][y] = 0;
+  //   }
+  // }
+
+  for (int i = 0; i < GEN_NUMBER; i++)
   {
-    one_gen(game_board, newGB);
-    game_over = 1;
+    int cells_number;
+
+    for (int x = 0; x < GAME_BOARD_SIZE - 2; x++) //using x,y represents columns better
+    {
+      for (int y = 0; y < GAME_BOARD_SIZE - 2; y++)
+      {
+          cells_number = 0;
+
+          if (game_board[x-1][y-1] == 1) {cells_number++;}
+          if (game_board[x][y-1] == 1) {cells_number++;}
+          if (game_board[x+1][y-1] == 1) {cells_number++;}
+          if (game_board[x-1][y] == 1) {cells_number++;}
+          if (game_board[x+1][y] == 1) {cells_number++;}
+          if (game_board[x-1][y+1] == 1) {cells_number++;}
+          if (game_board[x][y+1] == 1) {cells_number++;}
+          if (game_board[x+1][y+1] == 1) {cells_number++;}
+          //checking and populating the new game board
+          if (cells_number < 2)
+          {
+            newGB[x][y] = 0;
+          }
+          else if (cells_number == 2 || cells_number == 3)
+          {
+            newGB[x][y] = 1;
+          }
+          else if (cells_number > 3)
+          {
+            newGB[x][y] = 0;
+          }
+      }
+    }
+    //transfering the new game board to the old one
+    for (int x = 0; x < GAME_BOARD_SIZE - 2; x++)
+    {
+      for (int y = 0; y < GAME_BOARD_SIZE - 2; y++)
+      {
+        game_board[x][y] = newGB[x][y];
+      }
+    }
+
+    for (int x = 0; x < GAME_BOARD_SIZE - 2; x++)
+    {
+      for (int y = 0; y < GAME_BOARD_SIZE - 2; y++)
+      {
+        if (game_board[x][y] == 1)//replaces the 1s and 0s with a nicer looking thing
+        {
+          cout << " " << "#" << " ";
+        }
+        else
+        {
+          cout << " " << "." << " ";
+        }
+      }
+      cout << endl;
+    }
+    cout << endl;
   }
 
   return 0;
-}
-
-void one_gen(int game_board[][GAME_BOARD_SIZE], int newGB[][GAME_BOARD_SIZE])
-{
-  int cells_number;
-  for (int x = 0; x < GAME_BOARD_SIZE - 2; x++) //using x,y represents columns better
-  {
-    for (int y = 0; y < GAME_BOARD_SIZE - 2; y++)
-    {
-        cells_number = 0;
-
-        if (game_board[x-1][y-1] == 1) {cells_number++;}
-        if (game_board[x][y-1] == 1) {cells_number++;}
-        if (game_board[x+1][y-1] == 1) {cells_number++;}
-        if (game_board[x-1][y] == 1) {cells_number++;}
-        if (game_board[x+1][y] == 1) {cells_number++;}
-        if (game_board[x-1][y+1] == 1) {cells_number++;}
-        if (game_board[x][y+1] == 1) {cells_number++;}
-        if (game_board[x+1][y+1] == 1) {cells_number++;}
-
-        record(newGB, x, y, cells_number);
-    }
-  }
-
-  transfer(game_board, newGB);
-}
-
-void record(int newGB[][GAME_BOARD_SIZE], int x, int y, int cells_number)//records the dead or alive cells
-{
-  if (cells_number < 2)
-  {
-    newGB[x][y] = 0;
-  }
-  else if (cells_number == 2 || cells_number == 3)
-  {
-    newGB[x][y] = 1;
-  }
-  else if (cells_number > 3)
-  {
-    newGB[x][y] = 0;
-  }
-}
-
-void transfer (int game_board[][GAME_BOARD_SIZE], int newGB[][GAME_BOARD_SIZE])
-{
-  for (int x = 0; x < GAME_BOARD_SIZE; x++)
-  {
-    for (int y = 0; y < GAME_BOARD_SIZE; y++)
-    {
-      game_board[x][y] = newGB[x][y];
-    }
-  }
 }

37658292e5c7ca5ea5bcd1f1d4baf10f1405b73e
Leon Katz <leonkatz21@gmail.com>
2019-04-20 16:36:37 -0700

    Core logic still not working, the rest is ok

--- Game-of-Life.cpp
+++ Game-of-Life.cpp
@@ -22,68 +22,86 @@ int main (int argc, char* argv[])
 
 
   char game_board[GAME_BOARD_SIZE][GAME_BOARD_SIZE];
-  char newGB[GAME_BOARD_SIZE][GAME_BOARD_SIZE];
+  char temp_game_board[GAME_BOARD_SIZE][GAME_BOARD_SIZE];
 
   bool game_over = 0;
 
-  ifstream config(argv[1]);
+  ifstream config;
+  config.open(argv[1]);
 
-  for (int y = 0; y < GAME_BOARD_SIZE - 2; y++)
+// initial config
+  for (int y = 0; y < GAME_BOARD_SIZE; y++)
   {
-    for (int x = 0; x < GAME_BOARD_SIZE - 2; x++)
+    for (int x = 0; x < GAME_BOARD_SIZE; x++)
     {
-      config >> game_board[x][y];
-      cout << game_board[x][y];
+      config >> game_board[x][y]; // loads the config file
+      if (game_board[x][y] == LIVE_CHAR)//replaces the 1s and 0s with a nicer looking thing
+      {
+        cout << " " << "#" << " ";
+      }
+      else
+      {
+        cout << " " << "." << " ";
+      }
     }
+    cout << endl;
   }
+  cout << "Initial Configuration" << endl << endl;
 
+//generation based for loop, arg2 is the number of gens
   for (int i = 0; i < stoi(argv[2]); i++)
   {
-    int cells_number;
-
-    for (int y = 0; y < GAME_BOARD_SIZE - 2; y++) //using x,y represents columns better
+    for (int y = 0; y < GAME_BOARD_SIZE; y++) //using x,y represents columns better
     {
-      for (int x = 0; x < GAME_BOARD_SIZE - 2; x++)
+      for (int x = 0; x < GAME_BOARD_SIZE; x++)
       {
-          cells_number = 0;
+          int cells_number = 0;
 
-          if (game_board[x-1][y-1] == 1) {cells_number++;}
-          if (game_board[x][y-1] == 1) {cells_number++;}
-          if (game_board[x+1][y-1] == 1) {cells_number++;}
-          if (game_board[x-1][y] == 1) {cells_number++;}
-          if (game_board[x+1][y] == 1) {cells_number++;}
-          if (game_board[x-1][y+1] == 1) {cells_number++;}
-          if (game_board[x][y+1] == 1) {cells_number++;}
-          if (game_board[x+1][y+1] == 1) {cells_number++;}
-
-          //checking and populating the new game board
+          //core logic (IT'S KIND OF BROKEN)
+          if (game_board[x-1][y-1] == '1') {cells_number++;}
+          if (game_board[x][y-1] == '1') {cells_number++;}
+          if (game_board[x+1][y-1] == '1') {cells_number++;}
+          if (game_board[x-1][y] == '1') {cells_number++;}
+          if (game_board[x+1][y] == '1') {cells_number++;}
+          if (game_board[x-1][y+1] == '1') {cells_number++;}
+          if (game_board[x][y+1] == '1') {cells_number++;}
+          if (game_board[x+1][y+1] == '1') {cells_number++;}
+          //    Any live cell with a cells number of 2 live neighbours dies (becomes 0)
+          //     Any live cell with a cells number of two or three lives on (stays 1)
+          //     Any live cell a cells number of  more than 3 dies (becomes 0)
+          //     Any dead cell with a cells number of exactly 3 l a live cell (becomes1)
+          //checking and populating the new game board (part of core logic)
           if (cells_number < 2)
           {
-            newGB[x][y] = DEAD_CHAR;
+            temp_game_board[x][y] = DEAD_CHAR;
           }
-          else if (cells_number == 2 || cells_number == 3)
+          else if (cells_number == 3)
           {
-            newGB[x][y] = LIVE_CHAR;
+            temp_game_board[x][y] = LIVE_CHAR;
+          }
+          else if (cells_number == 2)
+          {
+            temp_game_board[x][y] = LIVE_CHAR;
           }
           else if (cells_number > 3)
           {
-            newGB[x][y] = DEAD_CHAR;
+            temp_game_board[x][y] = DEAD_CHAR;
           }
       }
     }
 
-    //transfering the new game board to the old one
-    for (int y = 0; y < GAME_BOARD_SIZE - 2; y++)
+    //transfering the temp_game_board  to the original
+    for (int y = 0; y < GAME_BOARD_SIZE; y++)
     {
-      for (int x = 0; x < GAME_BOARD_SIZE - 2; x++)
+      for (int x = 0; x < GAME_BOARD_SIZE; x++)
       {
-        game_board[x][y] = newGB[x][y];
+        game_board[x][y] = temp_game_board[x][y];
       }
     }
 
-    for (int y = 0; y < GAME_BOARD_SIZE - 2; y++)
+    for (int y = 0; y < GAME_BOARD_SIZE; y++)
     {
-      for (int x = 0; x < GAME_BOARD_SIZE - 2; x++)
+      for (int x = 0; x < GAME_BOARD_SIZE; x++)
       {
         if (game_board[x][y] == LIVE_CHAR)//replaces the 1s and 0s with a nicer looking thing
         {
@@ -96,7 +114,7 @@ int main (int argc, char* argv[])
       }
       cout << endl;
     }
-    cout << endl;
+    cout << "Gen " << i << endl << endl;
   }
 
   return 0;
